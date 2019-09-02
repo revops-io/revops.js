697c986626badd73016637efa8e9d243ab6f0e33
Chris Berns <cberns@revops.io>
2019-09-02 16:32:20 -0700

    wip payment types

---
+++ src/StripeForm.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import './styles.css'
+
+const defaultStyles = {
+  background: "#FFFFFF",
+  border: "1px solid #CED7E6",
+  boxSizing: "border-box",
+  borderRadius: "4px",
+  height:  "40px",
+  padding: "0 16px"
+};
+
+export default class StripeForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      errors: 0,
+      errors: false,
+    }
+    this.form = {};
+  }
+
+  static propTypes = {
+    styles: PropTypes.object,
+    onComplete: PropTypes.func,
+  }
+
+  componentDidMount() {
+    const script = document.createElement("script")
+
+    script.src = "https://js.verygoodvault.com/vgs-collect/1/ACkcn4HYv7o2XoRa7idWwVEX.js"
+    script.async = true
+    script.onload = () => {
+      this.initialize()
+    }
+    document.body.appendChild(script);
+  }
+
+  initialize = () => {
+    const styles = this.props.styles === undefined ? defaultStyles : this.props.styles
+
+    const form = VGSCollect.create("tnt6ryfiprp", function (state) { });
+    form.field("#customer-name .field-space", {
+      type: "text",
+      name: "name",
+      placeholder: "Pat Smalley",
+      validations: ["required"],
+      css: styles
+    });
+    form.field("#customer-email .field-space", {
+      type: "text",
+      name: "email",
+      placeholder: "patsmalley@company.com",
+      validations: ["required"],
+      css: styles
+    });
+    form.field("#customer-phone .field-space", {
+      type: "text",
+      name: "phone",
+      placeholder: "(800)-555-5555",
+      validations: ["required"],
+      css: styles
+    });
+
+    this.form = form
+
+  }
+
+  handleError = (errors) => this.setState({
+    errors
+  })
+
+  onSubmit = () => {
+
+    this.form.submit(
+      "/post",
+      {
+        headers: {
+          "x-custom-header": "Oh yes. I am a custom header"
+        }
+      },
+      function (status, data) {
+        if (this.props.onSubmit !== false) {
+          this.props.onComplete()
+        }
+      },
+      function (errors) {
+        () => this.handleError(errors)
+      }
+    )
+    this.props.onNext()
+  }
+
+  buttonGrp = () => {
+    const { onLast, onCancel, finalStep } = this.props
+    return (
+      <div>
+        <button
+          id="form-cancel-btn"
+          className="ui left floated button secondary basic"
+          onClick={() => onCancel()}>Cancel</button>
+        <button
+          id="form-next-btn"
+          className="ui right floated button positive"
+          onClick={this.onSubmit}>{finalStep ? 'Submit' : 'Next'}</button>
+        <button
+          id="form-prev-btn"
+          className="ui right floated button positive basic"
+          onClick={() => onLast()}>Previous</button>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <section>
+        <h1>Stripe</h1>
+        <form id="contact-form" className="ui form">
+          <div id="customer-name" className="field">
+            <label>Name</label>
+            <span className="field-space"></span>
+          </div>
+
+          <div id="customer-email" className="field">
+            <label >Email</label>
+            <span className="field-space"></span>
+          </div>
+
+          <div id="customer-phone" className="field">
+            <label>Phone</label>
+            <span className="field-space"></span>
+          </div>
+
+        </form>
+        {this.buttonGrp()}
+      </section>
+    )
+  }
+}
