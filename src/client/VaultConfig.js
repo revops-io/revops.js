8421aa6d3fbff61f011a5a088832248663bb4f25
Adam Ballai <adam@revops.io>
2019-09-02 18:21:57 -0700

    Update RevOps backend when contact info filled out

--- src/client/VaultConfig.js
+++ src/client/VaultConfig.js
@@ -1,2 +1,4 @@
-export const REVOPS_VAULT_COLLECT = 'https://js.verygoodvault.com/vgs-collect/1/ACvYkoDARh7Ajf3DhktqckgY.js'
-export const REVOPS_VAULT_ID = 'tnt2w1xznia'
+//export const REVOPS_VAULT_COLLECT = 'https://js.verygoodvault.com/vgs-collect/1/ACvYkoDARh7Ajf3DhktqckgY.js'
+export const REVOPS_VAULT_COLLECT = 'https://js.verygoodvault.com/vgs-collect/1/ACkcn4HYv7o2XoRa7idWwVEX.js'
+//export const REVOPS_VAULT_ID = 'tnt2w1xznia'
+export const REVOPS_VAULT_ID = 'tnt6ryfiprp'
