bb2a52eae040ee37da9580572cd1df13954f623a
Adam Ballai <adam@revops.io>
2019-09-05 17:21:58 -0700

    point to staging

--- src/client/VaultConfig.js
+++ src/client/VaultConfig.js
@@ -1,6 +1,6 @@
 //export const REVOPS_VAULT_COLLECT = 'https://js.verygoodvault.com/vgs-collect/1/ACvYkoDARh7Ajf3DhktqckgY.js'
-export const REVOPS_VAULT_COLLECT = 'https://js.verygoodvault.com/vgs-collect/1/ACkcn4HYv7o2XoRa7idWwVEX.js' // local testing
-//export const REVOPS_VAULT_COLLECT = 'https://js.verygoodvault.com/vgs-collect/1/ACvYkoDARh7Ajf3DhktqckgY.js' // staging testing
+//export const REVOPS_VAULT_COLLECT = 'https://js.verygoodvault.com/vgs-collect/1/ACkcn4HYv7o2XoRa7idWwVEX.js' // local testing
+export const REVOPS_VAULT_COLLECT = 'https://js.verygoodvault.com/vgs-collect/1/ACvYkoDARh7Ajf3DhktqckgY.js' // staging testing
 //export const REVOPS_VAULT_ID = 'tnt2w1xznia'
-export const REVOPS_VAULT_ID = 'tnt6ryfiprp' // local testing
-//export const REVOPS_VAULT_ID = 'tnttz8hit8p' // staging testing
+//export const REVOPS_VAULT_ID = 'tnt6ryfiprp' // local testing
+export const REVOPS_VAULT_ID = 'tnttz8hit8p' // staging testing
