c4932d9eee6e51ace29a441bc3265ba775529b7f
Chris Berns <cberns@revops.io>
2019-09-14 15:33:51 -0700

    improved coverage for credit card component

--- src/CreditCardForm.test.js
+++ src/CreditCardForm.test.js
@@ -10,8 +10,9 @@ describe('', () => {
       onLast: jest.fn(),
       onNext: jest.fn(),
       onError: jest.fn(),
-      accountModel: {
+      account: {
         id: "test",
+        billingPreferences: {},
         saveWithSecureForm: jest.fn(),
       },
       ...props,
@@ -23,5 +24,46 @@ describe('', () => {
     expect(wrapper.length).to.equal(1)
     expect(wrapper.find('section').length).to.equal(1)
     expect(wrapper.find('ButtonGroup').length).to.equal(1)
+    expect(wrapper.find('#cc-form').length).to.equal(1)
+    expect(wrapper.find('#cc-holder').length).to.equal(1)
+    expect(wrapper.find('#cc-exp').length).to.equal(1)
+    expect(wrapper.find('#cc-cvc').length).to.equal(1)
+  })
+
+  it('should not render button group when saveRef is defined', () => {
+    const mockProps = generateMockProps({saveRef: {}})
+    const wrapper = shallow(<CreditCardForm  {...mockProps} />)
+    expect(wrapper.find('ButtonGroup').length).to.equal(0)
+  })
+
+  it('should should transition to the ACH option', () => {
+    const mockProps = generateMockProps({
+      showACHLink: true,
+      changePaymentMethod: jest.fn(), 
+    })
+    const wrapper = shallow(<CreditCardForm  {...mockProps} />)
+    const achLink = wrapper.find('a.pay-by-ach-link')
+    expect(achLink.length).to.equal(1)
+    achLink.simulate('click')
+    expect(mockProps.changePaymentMethod.call.length).to.equal(1)
+  })
+
+  it('should submit correctly', () => {
+    const mockProps = generateMockProps({})
+    const wrapper = shallow(<CreditCardForm  {...mockProps} />)
+
+    wrapper.setState({
+      errors: null,
+      loading: null,
+      status: null,
+      response: null,
+    })
+
+    wrapper.instance().onSubmit()
+    expect(wrapper.instance().state.errors).to.equal(false)
+    expect(wrapper.instance().state.loading).to.equal(true)
+    expect(wrapper.instance().state.status).to.equal(false)
+    expect(wrapper.instance().state.response).to.equal(false)
+    expect(mockProps.account.saveWithSecureForm.call.length).to.equal(1)
   })
 })
