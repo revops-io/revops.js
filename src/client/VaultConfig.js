6d885d3cd42a536ba69efb09a00c8c40c2f2f76d
Adam Ballai <adam@revops.io>
2019-09-16 00:15:18 -0700

    Remove alternative configs

--- src/client/VaultConfig.js
+++ src/client/VaultConfig.js
@@ -10,34 +10,9 @@ const production = () => ({
   serviceName: 'revops-js-production',
 })
 
-const staging = () => ({
-  name: 'staging',
-  vaultCollectUrl: 'https://js.verygoodvault.com/vgs-collect/1/ACvYkoDARh7Ajf3DhktqckgY.js',
-  revopsBaseUrl: 'https://vault.revops.io',
-  plaidUrl: 'https://cdn.plaid.com/link/v2/stable/link-initialize.js',
-  plaidEnvironment: 'development',
-  plaidKey: 'c648203cbd9ce4b7ea39f26c61f115',
-  vaultId: 'tnttz8hit8p',
-  baseUrl: `https://${document.location.host}`,
-  serviceName: 'revops-js-staging',
-})
-
-const local = () => ({
-  name: "local",
-  vaultCollectUrl: 'https://js.verygoodvault.com/vgs-collect/1/ACvYkoDARh7Ajf3DhktqckgY.js',
-  revopsBaseUrl: 'https://vault.revops.io',
-  plaidUrl: 'https://cdn.plaid.com/link/v2/stable/link-initialize.js',
-  plaidEnvironment: 'sandbox',
-  plaidKey: 'c648203cbd9ce4b7ea39f26c61f115',
-  vaultId: 'tntje9m2ybe',
-  baseUrl: `https://${document.location.host}`,
-  serviceName: 'revops-js-local',
-})
 
 const configurations = () => ({
   "production": production(),
-  "staging": staging(),
-  "localhost": local(),
 })
 
 export const configure = (env = false) => {
@@ -48,17 +23,6 @@ export const configure = (env = false) => {
     }
 
     return environments[env]
-  }
-
-  // For testing
-  if(!!document !== true || !!document.domain !== true) {
-    return environments['localhost']
-  }
-
-  if(!!document !== false && document.domain.endsWith('staging.bill.sh')) {
-    return environments['staging']
-  } else if (document.domain.endsWith('localhost')) {
-    return environments['localhost']
   }
 
   return environments['production']
