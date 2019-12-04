d82dbbf54ad3ceed6186ac65b9028ecfd2b710c2
Chris Berns <cberns@revops.io>
2019-12-04 15:43:45 -0800

    initial fix

--- src/PlaidForm.js
+++ src/PlaidForm.js
@@ -416,6 +416,36 @@ export default class PlaidForm extends Component {
     </a>
   )
 
+  getCCLink = () => {
+    const {
+      showCardLink = true,
+      creditCardLink = this.creditCardLink(),
+    } = this.props
+
+    if(showCardLink === false){
+      return null
+    }
+    return creditCardLink
+  }
+  
+
+  getACHLink = () => {
+    const {
+      showACHLink = true,
+      achLink = this.achLink(), 
+    } = this.props
+
+    if(showACHLink === false){
+      return null
+    }
+    return achLink
+  } 
+
+  achLink = () => (
+    <TogglePlaid style={this.props.linkStyling} togglePlaidHandler={this.props.togglePlaidHandler} />
+  )
+
+
   render() {
     const { errors, } = this.state
     const {
@@ -446,9 +476,7 @@ export default class PlaidForm extends Component {
         }
         <section style={this.getSectionDisplayProps()}>
           {plaidLabel}
-          {showCardLink === true &&
-            !!creditCardLink === true ? creditCardLink : this.creditCardLink()
-          }
+          {this.getCCLink()}
           <button
             className="btn-primary centered single"
             style={this.props.buttonStylesPrimary}
@@ -478,16 +506,7 @@ export default class PlaidForm extends Component {
               {...propHelper.overrideFieldProps("bank-name")}
             />
           </div>
-
-          {showACHLink === true && !!achLink === true
-            ? achLink
-            : <TogglePlaid
-              style={this.props.linkStyling}
-              togglePlaidHandler={this.props.togglePlaidHandler}
-              plaidSelected={true}
-            />
-          }
-
+          {this.getACHLink()}
           <div className="ui clearing divider"></div>
           {!!this.props.saveRef === false &&
             <ButtonGroup
